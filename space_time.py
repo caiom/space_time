6e57920dfd26e14369fb66c8c995b76e52d0f3cc
Caio Mendes <caioctmendes@gmail.com>
2020-02-02 15:44:11 -0300

    Adicao de alguns comentarios

--- space_time.py
+++ space_time.py
@@ -20,14 +20,17 @@ def flood_fill(mapa, initial_y, initial_x, fill_value=2, target_value=1):
     
     height, width = mapa.shape[:2]
     
+    # Variavel que guarda todas as posicoes que devem ser checadas
     queue = [(initial_y, initial_x)]
     
     found_number = 0
     
+    # Enquanto ainda existir uma posicao a checar
     while len(queue):
         
         y, x = queue.pop()
         
+        # A posicao atual eh valida?
         if y > (height - 1) or x > (width - 1):
             continue
         
@@ -40,6 +43,7 @@ def flood_fill(mapa, initial_y, initial_x, fill_value=2, target_value=1):
         mapa[y, x] = fill_value
         found_number += 1
         
+        # Adicona os 4 vizinhos para checagem
         queue.append((y+1, x))
         queue.append((y, x+1))
         queue.append((y-1, x))
@@ -51,6 +55,7 @@ def numero_ilhas(mapa):
     '''Devolve o numero de ilhas no mapa (considere vizinhança 4-conexa)'''
     
     mapa_np = np.atleast_2d(mapa)
+    # Comparacao de float eh problematica
     mapa_np = mapa_np.astype(np.int64)
     
     height, width = mapa_np.shape[:2]
@@ -73,6 +78,7 @@ def quantidade_de_terra_afetada(mapa, i, j):
     semente lançada em mapa[i, j] (considere vizinhança 4-conexa)'''
     
     mapa_np = np.atleast_2d(mapa)
+    # Comparacao de float eh problematica
     mapa_np = mapa_np.astype(np.int64)
     
     return flood_fill(mapa_np, i, j, 2)
